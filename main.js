0247ff01f070e4165b0298afe34d8af7b2183a4a
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2024-02-14 16:50:57 +0300

    done

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById("app");
+let app1 = document.getElementById("app1");
+let app2 = document.getElementById("app2");
+let app3 = document.getElementById("app3");
+let Exchange = document.getElementById("Exchange");
+let pill = document.getElementById("pill");
+
+function add() {
+    app.value + app1.value
+    app2.innerHTML = app.value / app1.value;
+}
+function add2(){
+    app3.innerHTML= pill.value  * Exchange.value;
+}
